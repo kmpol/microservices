83f22bdd587176ee5bd4482a9fa6b2a1b6ac40d7
Karol <malickikarol0@gmail.com>
2023-12-07 21:19:28 +0100

    Added Zipkin and Sleuth to microservices

--- notification/src/main/java/pl/karol/notification/controller/NotificationController.java
+++ notification/src/main/java/pl/karol/notification/controller/NotificationController.java
@@ -1,6 +1,7 @@
 package pl.karol.notification.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import pl.karol.clients.notification.NotificationRequest;
 import pl.karol.notification.service.NotificationService;
@@ -8,12 +9,14 @@ import pl.karol.notification.service.NotificationService;
 @RestController
 @RequestMapping("api/v1/notification")
 @RequiredArgsConstructor
+@Slf4j
 public class NotificationController {
 
     private final NotificationService notificationService;
 
     @PostMapping
     public void saveNotification(@RequestBody NotificationRequest notificationRequest) {
+        log.info("Notification request POST wuth data: {}", notificationRequest);
         notificationService.sendNotification(notificationRequest);
     }
 
